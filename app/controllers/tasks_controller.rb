877fe7fc5246eb6cbd9520cd608acc6684cc1ab0
Anahita Hekmat <hekmatana@gmail.com>
2023-11-14 15:28:56 +0100

    finish

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,7 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
     @tasks = Task.all
   end
 
-  def show
-  end
+  def show ; end
 
   def new
     @task = Task.new
@@ -31,7 +30,9 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
+
   private
+
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
@@ -39,5 +40,4 @@ before_action :set_task, only: [:show, :edit, :update, :destroy]
   def set_task
     @task = Task.find(params[:id])
   end
-
 end
